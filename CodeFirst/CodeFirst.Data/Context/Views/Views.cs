0086c5e8d9064ed06968f54357f743e161777d06
Gokce Ozel <gokce.ozel@alogmedi.com>
2021-02-21 19:13:01 +0300

    Create Extensions

---
+++ CodeFirst/CodeFirst.Data/Context/Views/Views.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFirst.Data.Context.Views
+{
+    public class Views
+    {
+    }
+}
---
+++ CodeFirst/CodeFirst.Data/Extensions/IServiceCollectionExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using CodeFirst.Data.Context;
+
+namespace CodeFirst.Data.Extensions
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection AddSchoolDbContext(this IServiceCollection services)
+        {
+            services
+                .AddDbContext<SchoolContext>(options =>
+                {
+                    IConfiguration configuration = services.BuildServiceProvider().GetService<IConfiguration>();
+                    options.UseSqlServer(configuration["ConnectionStrings:School"], x =>
+                    {
+                        x.MigrationsHistoryTable("EFMigrationsHistory", "School");
+                    });
+                })
+                .AddScoped<SchoolContext>();
+            return services;
+        }
+    }
+}
