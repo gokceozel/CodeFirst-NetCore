e76abfefc9adff2a06c6f31ccade67b8c428029d
Gokce Ozel <gokce.ozel@alogmedi.com>
2021-02-21 21:57:10 +0300

    Create Context Factory

--- CodeFirst/CodeFirst.API/School/Context/SchoolContextFactory.cs
+++ CodeFirst/CodeFirst.API/School/Context/SchoolContextFactory.cs
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CodeFirst.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace CodeFirst.API.School.Context
 {
-    public class SchoolContextFactory
+    public class SchoolContextFactory : IDesignTimeDbContextFactory<SchoolContext>
     {
+        public SchoolContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configurationRoot.GetValue<string>("ConnectionStrings:School");
+
+            var optionBuilder = new DbContextOptionsBuilder<SchoolContext>();
+
+            optionBuilder.UseSqlServer(connectionString);
+
+            return new SchoolContext(optionBuilder.Options);
+           
+        }
     }
 }
